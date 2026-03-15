33a573a60dd748d15d50be1f933b96dac33edeaa
magnificolv <magnificox@gmail.com>
2026-03-15 16:36:14 +0200

    new

--- data/content.js
+++ data/content.js
@@ -71,7 +71,7 @@ const CATEGORIES = [
     name:  'Physical',
     sub:   'Ķermeņa iepazīšana',
     bg:    '#1c0808',
-    image: null, // Nomainī uz: 'images/physical.png'
+    image: 'images/physical.jpg',
     items: [
       { icon: '🔥', title: 'Kundalini & enerģija',    desc: 'Elpošanas prakse, kundalini joga, enerģijas pamošanās' },
       { icon: '🌹', title: 'Seksuālā pašizzināšana',  desc: 'Sava ķermeņa, vēlmju un enerģijas izpēte' },
